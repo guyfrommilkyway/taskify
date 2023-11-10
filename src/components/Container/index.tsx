64daca688bfcbe06b216e10bf61b6f22efce3c3b
Almer Tampus <almerflorestampus@gmail.com>
2023-11-11 07:51:31 +0800

    Set maximum width at 1920px

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Container: React.FC<ContainerProps> = props => {
   const { children } = props;
-  return <main className='w-full mx-auto p-4 md:p-8 lg:p-10'>{children}</main>;
+  return <main className='w-full max-w-[1920px] mx-auto p-4 md:p-8 lg:p-10'>{children}</main>;
 };
 
 export default Container;
