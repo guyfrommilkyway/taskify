ddd9270e42f1d48463ab303182ea42fe7c529de3
Almer Tampus <almerflorestampus@gmail.com>
2023-11-11 07:46:33 +0800

    Update types

--- src/types/task.d.ts
+++ src/types/task.d.ts
@@ -1,8 +1,35 @@
-interface TaskListProps {
-  title: Required<string>;
-}
-
 interface Task {
   id: Required<string>;
   text: Required<string>;
+  status: Required<boolean>;
+}
+
+interface TaskListProps {
+  id: Required<string>;
+  title: Required<string>;
+  data: Required<Task[]>;
+}
+
+interface TaskItemProps {
+  id: Required<string>;
+  index: Required<number>;
+  text: Required<string>;
+  status: Required<boolean>;
+}
+
+interface TaskButtonsProps {
+  id: Required<string>;
+  status: Required<boolean>;
+  isEditMode: Required<boolean>;
+  editModeHandler: () => void;
+}
+
+interface TaskInputProps {
+  inputText: Required<string>;
+  inputTextHandler: (param: string) => void;
+}
+
+interface TaskTextProps {
+  text: Required<string>;
+  status: boolean;
 }
