40f3098a22c1386f270345ce92f4a6b440ba66ed
Almer Tampus <almerflorestampus@gmail.com>
2023-11-12 02:19:23 +0800

    Check store for cache on initial load

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,23 @@
 // packages
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // components
 import Container from '@/components/Container';
 import Task from '@/components/Task';
+import useTaskContext from '@/hooks/useTaskContext';
 
 const Home: React.FC = () => {
+  const { taskHandler } = useTaskContext();
+
+  // will only run once
+  // during initial load
+  useEffect(() => {
+    taskHandler({ type: 'SET' });
+
+    // clean up function
+    return () => {};
+  });
+
   return (
     <Container>
       <Task />
